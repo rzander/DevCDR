903a4976400409926cb46190adb541c917a432a1
Roger Zander <roger@zander.ch>
2018-07-07 18:29:13 +0200

    New: GetMissingDriver and GetMissingOSUpdates

---
+++ Source/DevCDRServer/NET47/App_Data/PSScripts/GetDriverUpdates.ps1
@@ -0,0 +1,14 @@
+﻿$UpdateSvc = New-Object -ComObject Microsoft.Update.ServiceManager            
+$UpdateSvc.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"")  
+$Session = New-Object -ComObject Microsoft.Update.Session           
+$Searcher = $Session.CreateUpdateSearcher() 
+$Searcher.ServiceID = '7971f918-a847-4430-9279-4a52d1efe18d'
+$Searcher.SearchScope =  1 # MachineOnly
+$Searcher.ServerSelection = 3 # Third Party
+$Criteria = "IsInstalled=0 and Type='Driver'"
+$SearchResult = $Searcher.Search($Criteria)          
+$Updates = $SearchResult.Updates
+if($Update.count -gt 0) {
+$Updates | select Title, DriverModel, DriverVerDate, Driverclass, DriverManufacturer | convertto-json } else {
+	"No Driver-Updates found..."
+}
---
+++ Source/DevCDRServer/NET47/App_Data/PSScripts/GetMissingOSUpdates.ps1
@@ -0,0 +1,14 @@
+﻿#Check if PSWindowsUpdate Module is installed
+try {
+if(Get-InstalledModule -Name PSWindowsUpdate -MinimumVersion "2.0.0.4" -ea SilentlyContinue) {} else 
+{
+ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+ Set-PSRepository -Name PSGallery -InstallationPolicy Trusted 
+ Install-Module PSWindowsUpdate -Force
+ (Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -confirm:$false).Name
+}
+$upd = Get-WUList -MicrosoftUpdate
+if($upd.count -gt 0) {
+ $upd | select Title | ConvertTo-Json
+} else { "No missing Updates detected."}
+}catch { "Error, unable to detect Updates" }
